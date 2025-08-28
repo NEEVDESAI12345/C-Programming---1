43a2833490f4bd595f90f583b46b4328703db33b
NEEVDESAI12345 <desaineev246@gmail.com>
2025-08-28 23:46:49 +0530

    Create DOLLAR TO RUPEE.c

---
+++ DOLLAR TO RUPEE.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main() 
+{
+    float D,R;
+    
+    
+    printf("Enter DOLLAR");
+    scanf("%f",&D);
+
+    
+    R=48*D;
+
+    
+    printf ("rupee are %f",R);
+
+}
