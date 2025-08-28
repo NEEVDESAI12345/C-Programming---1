d76c47f2b23e66cd33c432cd7a08e2e7f54dbe4d
NEEVDESAI12345 <desaineev246@gmail.com>
2025-08-28 23:45:33 +0530

    Create RUPEE TO DOLLAR.c

---
+++ RUPEE TO DOLLAR.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main() 
+{
+    float D,R;
+    
+    
+    printf("Enter rupee");
+    scanf("%f",&R);
+
+    
+    D=R/48.0;
+
+    
+    printf ("amount in dollar is %f",D);
+
+}
