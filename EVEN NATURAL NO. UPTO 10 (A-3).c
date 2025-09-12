0014be276e68bd56938807f6c79fcb17214ad902
NEEVDESAI12345 <desaineev246@gmail.com>
2025-09-12 09:03:21 +0530

    Update EVEN NATURAL NO. UPTO 10 (A-3).c

--- EVEN NATURAL NO. UPTO 10 (A-3).c
+++ EVEN NATURAL NO. UPTO 10 (A-3).c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+    int n, i;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("The first odd natural numbers upto %d are:\n", n);
+
+    for(i = 1; i <= n; i=i+2) {
+        printf("%d\t", i);
+    }
+
+    return 0;
+}
