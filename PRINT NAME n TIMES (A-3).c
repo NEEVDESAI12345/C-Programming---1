7e513a6df8a5bd847ea7747bd800e508b84cf86e
NEEVDESAI12345 <desaineev246@gmail.com>
2025-09-12 09:00:22 +0530

    Create PRINT NAME n TIMES (A-3).c

---
+++ PRINT NAME n TIMES (A-3).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+    int i,n;
+    
+    printf("no.of times to print name: ");
+    scanf("%d",&n);
+    
+    for(i = 1; i <= n; i++) {
+   printf("%d) NEEV\n",i);
+    }
+    return 0;
+}
